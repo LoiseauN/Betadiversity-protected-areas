e838bd909d645bbe8503df5e5d42b957969b725b
LoiseauN <nicolas.loiseau@cnrs.fr>
2021-03-05 17:47:07 +0100

    :boom: Figure 2 & 3

--- Outputs/Figure_2/figure_2.R
+++ Outputs/Figure_2/figure_2.R
@@ -1,83 +1,78 @@
-bird <- na.omit(data.frame(dbrda_BIRDS_close_50km))
 
-Intersect_bird     <- bird$RsquareAdj_all-bird$RsquareAdj_partial_PA-bird$RsquareAdj_partial_hab
-Pa_perc_bird          <- bird$RsquareAdj_partial_PA/(bird$RsquareAdj_all+Intersect_bird)
-Hab_perc_bird         <- bird$RsquareAdj_partial_hab/(bird$RsquareAdj_all+Intersect_bird)
-Intersect_perc_bird   <- Intersect_bird  /(bird$RsquareAdj_all+Intersect_bird)
+## Library ----
 
-metaRNASeq::fishercomb(bird$Pr..F._mod_partial_PA) 
-metaRNASeq::fishercomb(bird$Pr..F._mod_partial_hab) 
+library(stringr)
+library(ggplot2)
+library(grid)
+library(png)
+## Source multiplot ----
 
-fish <- data.frame(rbind(dbrda_FISH_RLS_50km,dbrda_fish_close_50km_SERF))
+source(here::here("Function", "multiplot.R"))
 
-Intersect_fish     <- fish$RsquareAdj_all-fish$RsquareAdj_partial_PA-fish$RsquareAdj_partial_hab
-Pa_perc_fish          <- fish$RsquareAdj_partial_PA/(fish$RsquareAdj_all+Intersect_fish)
-Hab_perc_fish         <- fish$RsquareAdj_partial_hab/(fish$RsquareAdj_all+Intersect_fish)
-Intersect_perc_fish   <- Intersect_fish  /(fish$RsquareAdj_all+Intersect_fish)
+## Import Icons ----
 
-metaRNASeq::fishercomb(fish$Pr..F._mod_partial_PA) 
-metaRNASeq::fishercomb(fish$Pr..F._mod_partial_hab) 
+paths <- list.files(path = here::here("Outputs", "Figure_2"), pattern = "*.png$", 
+                    full.names = TRUE)
 
+files <- list.files(path = here::here("Outputs", "Figure_2"), pattern = "*.png$", 
+                    full.names = FALSE)
 
-
-plant <- data.frame(dbrda_PLANTS_close_50km)
-
-Intersect_plant     <- plant$RsquareAdj_all-plant$RsquareAdj_partial_PA-plant$RsquareAdj_partial_hab
-Pa_perc_plant          <- plant$RsquareAdj_partial_PA/(plant$RsquareAdj_all+Intersect_plant)
-Hab_perc_plant         <- plant$RsquareAdj_partial_hab/(plant$RsquareAdj_all+Intersect_plant)
-Intersect_perc_plant   <- Intersect_plant  /(plant$RsquareAdj_all+Intersect_plant)
-
-metaRNASeq::fishercomb(plant$Pr..F._mod_partial_PA) 
-metaRNASeq::fishercomb(plant$Pr..F._mod_partial_hab) 
+all_im <- lapply(paths, png::readPNG)
+names(all_im) <- gsub(".png", "", files)
 
 
-df_res <- data.frame(Percentage = c(Pa_perc_fish*100,Pa_perc_bird*100,Pa_perc_plant*100,
-                                    Hab_perc_fish*100,Hab_perc_bird*100,Hab_perc_plant*100),
-                     Var = c(rep("PA",nrow(fish)),rep("PA",nrow(bird)),rep("PA",nrow(plant)),
-                             rep("HAB",nrow(fish)),rep("HAB",nrow(bird)),rep("HAB",nrow(plant))),
-                     taxa= c(rep("fish",nrow(fish)),rep("bird",nrow(bird)),rep("plant",nrow(plant)),
-                             rep("fish",nrow(fish)),rep("bird",nrow(bird)),rep("plant",nrow(plant)))) 
+## Prepare Data ----
 
-ggplot(data=df_res,
-       aes(x= taxa, y = Percentage,color=Var))+
-  geom_boxplot()
+filenames <- list.files(path = here::here("Outputs", "Figure_2"), pattern = ".RData$",
+                        full.names = FALSE)
 
+files <- list.files(path = here::here("Outputs", "Figure_2"), pattern = ".RData$",
+                    full.names = TRUE)
 
-df_res_Rsquare <- data.frame(Rsquare = c(fish$RsquareAdj_all,fish$RsquareAdj_partial_PA,fish$RsquareAdj_partial_hab,
-                                         bird$RsquareAdj_all,bird$RsquareAdj_partial_PA,bird$RsquareAdj_partial_hab,
-                                         plant$RsquareAdj_all,plant$RsquareAdj_partial_PA,plant$RsquareAdj_partial_hab),
-                             Var = c(rep("ALL",nrow(fish)),rep("PA",nrow(fish)),rep("HAB",nrow(fish)),
-                                     rep("ALL",nrow(bird)),rep("PA",nrow(bird)),rep("HAB",nrow(bird)),
-                                     rep("ALL",nrow(plant)),rep("PA",nrow(plant)),rep("HAB",nrow(plant))),
-                             taxa= c(rep("fish",nrow(fish)*3),rep("bird",nrow(bird)*3),rep("plant",nrow(plant)*3))) 
+lapply(files, load, environment())
 
 
-ggplot(data=df_res_Rsquare,
-       aes(x= taxa, y = Rsquare,color=Var))+
-  geom_boxplot()
+#--bird dbrda
+Intersect_bird     <- bird_dbrda$RsquareAdj_all-bird_dbrda$RsquareAdj_partial_PA-bird_dbrda$RsquareAdj_partial_hab
+Pa_perc_bird          <- bird_dbrda$RsquareAdj_partial_PA/(bird_dbrda$RsquareAdj_all+Intersect_bird)
+Hab_perc_bird         <- bird_dbrda$RsquareAdj_partial_hab/(bird_dbrda$RsquareAdj_all+Intersect_bird)
+Intersect_perc_bird   <- Intersect_bird  /(bird_dbrda$RsquareAdj_all+Intersect_bird)
+
+metaRNASeq::fishercomb(bird_dbrda$Pval_mod_partial_PA) 
+metaRNASeq::fishercomb(bird_dbrda$Pval_mod_partial_hab) 
+
+#--fish dbrda
+Intersect_fish     <- fish_dbrda$RsquareAdj_all-fish_dbrda$RsquareAdj_partial_PA-fish_dbrda$RsquareAdj_partial_hab
+Pa_perc_fish          <- fish_dbrda$RsquareAdj_partial_PA/(fish_dbrda$RsquareAdj_all+Intersect_fish)
+Hab_perc_fish         <- fish_dbrda$RsquareAdj_partial_hab/(fish_dbrda$RsquareAdj_all+Intersect_fish)
+Intersect_perc_fish   <- Intersect_fish  /(fish_dbrda$RsquareAdj_all+Intersect_fish)
+
+metaRNASeq::fishercomb(fish_dbrda$Pval_mod_partial_PA) 
+metaRNASeq::fishercomb(fish_dbrda$Pval_mod_partial_hab) 
 
 
+#--plant dbrda
+Intersect_plant     <- plant_dbrda$RsquareAdj_all-plant_dbrda$RsquareAdj_partial_PA-plant_dbrda$RsquareAdj_partial_hab
+Pa_perc_plant          <- plant_dbrda$RsquareAdj_partial_PA/(plant_dbrda$RsquareAdj_all+Intersect_plant)
+Hab_perc_plant         <- plant_dbrda$RsquareAdj_partial_hab/(plant_dbrda$RsquareAdj_all+Intersect_plant)
+Intersect_perc_plant   <- Intersect_plant  /(plant_dbrda$RsquareAdj_all+Intersect_plant)
 
-df_res <- data.frame(Percentage = c(Pa_perc_fish*100,Pa_perc_bird*100,Pa_perc_plant*100,
-                                    Hab_perc_fish*100,Hab_perc_bird*100,Hab_perc_plant*100),
-                     Var = c(rep("PA",nrow(fish)),rep("PA",nrow(bird)),rep("PA",nrow(plant)),
-                             rep("HAB",nrow(fish)),rep("HAB",nrow(bird)),rep("HAB",nrow(plant))),
-                     taxa= c(rep("fish",nrow(fish)),rep("bird",nrow(bird)),rep("plant",nrow(plant)),
-                             rep("fish",nrow(fish)),rep("bird",nrow(bird)),rep("plant",nrow(plant)))) 
+metaRNASeq::fishercomb(plant_dbrda$Pval_mod_partial_PA) 
+metaRNASeq::fishercomb(plant_dbrda$Pval_mod_partial_hab) 
 
-# ---
-data_fish <- data.frame(Percentage = c(Pa_perc_fish*100,
+# --- plot
+data_dbrda_fish <- data.frame(Percentage = c(Pa_perc_fish*100,
                                        Hab_perc_fish*100),
-                        Var = c(rep("Protection",nrow(fish)),
-                                rep("Habitat",nrow(fish))))
+                        Var = c(rep("Protection",nrow(fish_dbrda)),
+                                rep("Habitat",nrow(fish_dbrda))))
 
 #Pour conserver l'ordre des boxplot
-data_fish$Var<- factor(data_fish$Var,levels = c('Protection','Habitat'),ordered = TRUE)
+data_dbrda_fish$Var<- factor(data_dbrda_fish$Var,levels = c('Protection','Habitat'),ordered = TRUE)
 
 ###Fish
 my_grob1 = grobTree(textGrob("***", x=0.22,  y=0.97, hjust=0, gp=gpar(col="black", fontsize=24,fontface=2)))
 my_grob2 = grobTree(textGrob("***", x=0.67,  y=0.97, hjust=0, gp=gpar(col="black", fontsize=24,fontface=2)))
-p1 <- ggplot(data_fish, aes(Var,Percentage,colour=Var)) +
+p1 <- ggplot(data_dbrda_fish, aes(Var,Percentage,colour=Var)) +
   geom_boxplot(alpha=0,show.legend=FALSE)+  #mettre T pour afficher la legend
   geom_jitter(aes(Var,Percentage),
               position=position_jitter(width=0.05,height=0),
@@ -88,7 +83,7 @@ p1 <- ggplot(data_fish, aes(Var,Percentage,colour=Var)) +
        x = "",
        y = "Percentage",
        colour = "",size=15) + ylim(0,100)+
-  theme_bw() +  annotation_custom(rasterGrob(img),  xmin = 2, xmax = 2.5, ymin = 92, ymax = 102) +
+  theme_bw() +  annotation_custom(rasterGrob(all_im$Fish),  xmin = 2, xmax = 2.5, ymin = 92, ymax = 102) +
   theme(axis.title.y = element_text(size=16),axis.text.x = element_text(size=14))+
   annotation_custom(my_grob1)+
   annotation_custom(my_grob2)
@@ -98,16 +93,16 @@ p1
 # ---
 my_grob1 = grobTree(textGrob("***", x=0.22,  y=0.97, hjust=0, gp=gpar(col="black", fontsize=24,fontface=2)))
 my_grob2 = grobTree(textGrob("***", x=0.67,  y=0.97, hjust=0, gp=gpar(col="black", fontsize=24,fontface=2)))
-data_bird <- data.frame(Percentage = c(Pa_perc_bird*100,
+data_dbrda_bird <- data.frame(Percentage = c(Pa_perc_bird*100,
                                        Hab_perc_bird*100),
-                        Var = c(rep("Protection",nrow(bird)),
-                                rep("Habitat",nrow(bird))))
+                        Var = c(rep("Protection",nrow(bird_dbrda)),
+                                rep("Habitat",nrow(bird_dbrda))))
 
 #Pour conserver l'ordre des boxplot
-data_bird$Var<- factor(data_bird$Var,levels = c('Protection','Habitat'),ordered = TRUE)
+data_dbrda_bird$Var<- factor(data_dbrda_bird$Var,levels = c('Protection','Habitat'),ordered = TRUE)
 
 ###bird
-p2 <- ggplot(data_bird, aes(Var,Percentage,colour=Var)) +
+p2 <- ggplot(data_dbrda_bird, aes(Var,Percentage,colour=Var)) +
   geom_boxplot(alpha=0,show.legend=FALSE)+  #mettre T pour afficher la legend
   geom_jitter(aes(Var,Percentage),
               position=position_jitter(width=0.05,height=0),
@@ -118,25 +113,25 @@ p2 <- ggplot(data_bird, aes(Var,Percentage,colour=Var)) +
        x = "",
        y = " ",
        colour = "",size=15) + ylim(0,100)+
-  theme_bw() +  annotation_custom(rasterGrob(img2),  xmin = 2.1, xmax = 2.5, ymin = 92, ymax = 102) +
+  theme_bw() +  annotation_custom(rasterGrob(all_im$Bird),  xmin = 2.1, xmax = 2.5, ymin = 92, ymax = 102) +
   theme(axis.title.y = element_text(size=16),axis.text.x = element_text(size=14))+
   annotation_custom(my_grob1)+
   annotation_custom(my_grob2)
 p2
 
 # ---
-data_plant <- data.frame(Percentage = c(Pa_perc_plant*100,
+data_dbrda_plant <- data.frame(Percentage = c(Pa_perc_plant*100,
                                         Hab_perc_plant*100),
-                         Var = c(rep("Protection",nrow(plant)),
-                                 rep("Habitat",nrow(plant))))
+                         Var = c(rep("Protection",nrow(plant_dbrda)),
+                                 rep("Habitat",nrow(plant_dbrda))))
 
 #Pour conserver l'ordre des boxplot
-data_plant$Var<- factor(data_plant$Var,levels = c('Protection','Habitat'),ordered = TRUE)
+data_dbrda_plant$Var<- factor(data_dbrda_plant$Var,levels = c('Protection','Habitat'),ordered = TRUE)
 
 ###plant
 my_grob1 = grobTree(textGrob("***", x=0.22,  y=0.97, hjust=0, gp=gpar(col="black", fontsize=24,fontface=2)))
 my_grob2 = grobTree(textGrob("***", x=0.67,  y=0.97, hjust=0, gp=gpar(col="black", fontsize=24,fontface=2)))
-p3<- ggplot(data_plant, aes(Var,Percentage,colour=Var)) +
+p3<- ggplot(data_dbrda_plant, aes(Var,Percentage,colour=Var)) +
   geom_boxplot(alpha=0,show.legend=FALSE)+  #mettre T pour afficher la legend
   geom_jitter(aes(Var,Percentage),
               position=position_jitter(width=0.05,height=0),
@@ -147,120 +142,17 @@ p3<- ggplot(data_plant, aes(Var,Percentage,colour=Var)) +
        x = "",
        y = " ",
        colour = "",size=15) + ylim(0,100)+
-  theme_bw() +  annotation_custom(rasterGrob(img3),  xmin = 2.1, xmax = 2.5, ymin = 92, ymax = 102) +
+  theme_bw() +  annotation_custom(rasterGrob(all_im$Plant),  xmin = 2.1, xmax = 2.5, ymin = 92, ymax = 102) +
   theme(axis.title.y = element_text(size=16),axis.text.x = element_text(size=14))+
   annotation_custom(my_grob1)+
   annotation_custom(my_grob2)
 p3
 
+
+
+grDevices::pdf(file = here::here("Figures", "Figure2.pdf"), 
+               width = 12, height = 8) #SAVE A4
 multiplot(p1,p2,p3,cols=3)
+dev.off()
 
 #save 8*12
-
-
-
-
-
-
-##FOR SUP
-
-# ---
-data_fish <- data.frame(Rsquare = c(fish$RsquareAdj_all,fish$RsquareAdj_partial_PA,fish$RsquareAdj_partial_hab),
-                        Var = c(rep("All",nrow(fish)),
-                                rep("Protection",nrow(fish)),
-                                rep("Habitat",nrow(fish))))
-
-#Pour conserver l'ordre des boxplot
-data_fish$Var<- factor(data_fish$Var,levels = c('All','Protection','Habitat'),ordered = TRUE)
-data_fish$dist_cat_n[data_fish$Var == "All"] <- -0.25
-data_fish$dist_cat_n[data_fish$Var == "Protection"] <- 0.25
-data_fish$dist_cat_n[data_fish$Var == "Habitat"] <-  0 
-
-data_fish$scat_adj[data_fish$Var == "All"] <- 1.25
-data_fish$scat_adj[data_fish$Var == "Protection"] <- 1.75
-data_fish$scat_adj[data_fish$Var == "Habitat"] <- 3
-
-###Fish
-
-p4<- ggplot(data_fish, aes(Var,Rsquare,colour=Var)) +
-  geom_boxplot(alpha=0,show.legend=FALSE)+  #mettre T pour afficher la legend
-  geom_jitter(aes(dist_cat_n + scat_adj ,Rsquare),
-              position=position_jitter(width=0.05,height=0),
-              alpha=0.55, size=2,
-              show.legend=FALSE) +
-  scale_colour_manual(values=c("darkblue", "cyan4", "cyan3"))+
-  labs(title = "",
-       x = "",
-       y = "Rsquare",
-       colour = "",size=15) + ylim(0,0.4)+
-  theme_bw() +  annotation_custom(rasterGrob(img),  xmin = 2.8, xmax = 3.5, ymin = 0.35, ymax = 0.45) +
-  theme(axis.title.y = element_text(size=16),axis.text.x = element_text(size=14)) 
-
-
-# ---
-data_bird <- data.frame(Rsquare = c(bird$RsquareAdj_all,bird$RsquareAdj_partial_PA,bird$RsquareAdj_partial_hab),
-                        Var = c(rep("All",nrow(bird)),
-                                rep("Protection",nrow(bird)),
-                                rep("Habitat",nrow(bird))))
-
-#Pour conserver l'ordre des boxplot
-data_bird$Var<- factor(data_bird$Var,levels = c('All','Protection','Habitat'),ordered = TRUE)
-data_bird$dist_cat_n[data_bird$Var == "All"] <- -0.25
-data_bird$dist_cat_n[data_bird$Var == "Protection"] <- 0.25
-data_bird$dist_cat_n[data_bird$Var == "Habitat"] <-  0 
-
-data_bird$scat_adj[data_bird$Var == "All"] <- 1.25
-data_bird$scat_adj[data_bird$Var == "Protection"] <- 1.75
-data_bird$scat_adj[data_bird$Var == "Habitat"] <- 3
-
-###bird
-
-p5<- ggplot(data_bird, aes(Var,Rsquare,colour=Var)) +
-  geom_boxplot(alpha=0,show.legend=FALSE)+  #mettre T pour afficher la legend
-  geom_jitter(aes(dist_cat_n + scat_adj ,Rsquare),
-              position=position_jitter(width=0.05,height=0),
-              alpha=0.55, size=2,
-              show.legend=FALSE) +
-  scale_colour_manual(values=c("coral4", "chocolate1", "coral1"))+
-  labs(title = "",
-       x = "",
-       y = " ",
-       colour = "",size=15) + ylim(0,0.4)+
-  theme_bw() +  annotation_custom(rasterGrob(img2),  xmin = 2.8, xmax = 3.5, ymin = 0.35, ymax = 0.45)  +
-  theme(axis.title.y = element_text(size=16),axis.text.x = element_text(size=14))
-
-
-# ---
-data_plant <- data.frame(Rsquare = c(plant$RsquareAdj_all,plant$RsquareAdj_partial_PA,plant$RsquareAdj_partial_hab),
-                         Var = c(rep("All",nrow(plant)),
-                                 rep("Protection",nrow(plant)),
-                                 rep("Habitat",nrow(plant))))
-
-#Pour conserver l'ordre des boxplot
-data_plant$Var<- factor(data_plant$Var,levels = c('All','Protection','Habitat'),ordered = TRUE)
-data_plant$dist_cat_n[data_plant$Var == "All"] <- -0.25
-data_plant$dist_cat_n[data_plant$Var == "Protection"] <- 0.25
-data_plant$dist_cat_n[data_plant$Var == "Habitat"] <-  0 
-
-data_plant$scat_adj[data_plant$Var == "All"] <- 1.25
-data_plant$scat_adj[data_plant$Var == "Protection"] <- 1.75
-data_plant$scat_adj[data_plant$Var == "Habitat"] <- 3
-
-###plant
-
-p6<- ggplot(data_plant, aes(Var,Rsquare,colour=Var)) +
-  geom_boxplot(alpha=0,show.legend=FALSE)+  #mettre T pour afficher la legend
-  geom_jitter(aes(dist_cat_n + scat_adj ,Rsquare),
-              position=position_jitter(width=0.05,height=0),
-              alpha=0.55, size=2,
-              show.legend=FALSE) +
-  scale_colour_manual(values=c("aquamarine4", "chartreuse3", "aquamarine2"))+
-  labs(title = "",
-       x = "",
-       y = " ",
-       colour = "",size=15) + ylim(0,0.4)+
-  theme_bw() +  annotation_custom(rasterGrob(img3),  xmin = 2.8, xmax = 3.5, ymin = 0.35, ymax = 0.45)  +
-  theme(axis.title.y = element_text(size=16),axis.text.x = element_text(size=14))
-
-
-multiplot(p4,p5,p6,cols=3)
--- Outputs/Figure_3/figure_3.R
+++ Outputs/Figure_3/figure_3.R
@@ -6,7 +6,9 @@ library(stringr)
 library(ggplot2)
 library(grid)
 library(png)
+## Source multiplot ----
 
+source(here::here("Function", "multiplot.R"))
 ## Import Icons ----
 
 paths <- list.files(path = here::here("Outputs", "Figure 2"), pattern = "*.png$", 
@@ -21,51 +23,14 @@ names(all_im) <- gsub(".png", "", files)
 
 ## Prepare Data ----
 
-filenames <- list.files(path = here::here("Outputs", "Figure 2"), pattern = ".RData$",
+filenames <- list.files(path = here::here("Outputs", "Figure_3"), pattern = ".RData$",
                         full.names = FALSE)
 
-files <- list.files(path = here::here("Outputs", "Figure 2"), pattern = ".RData$",
+files <- list.files(path = here::here("Outputs", "Figure_3"), pattern = ".RData$",
                     full.names = TRUE)
 
 lapply(files, load, environment())
 
-
-## FONCTION  multiplot
-multiplot <- function(..., plotlist=NULL, file, cols=1, layout=NULL) {
-  library(grid)
-  
-  # Make a list from the ... arguments and plotlist
-  plots <- c(list(...), plotlist)
-  
-  numPlots = length(plots)
-  
-  # If layout is NULL, then use 'cols' to determine layout
-  if (is.null(layout)) {
-    # Make the panel
-    # ncol: Number of columns of plots
-    # nrow: Number of rows needed, calculated from # of cols
-    layout <- matrix(seq(1, cols * ceiling(numPlots/cols)),
-                     ncol = cols, nrow = ceiling(numPlots/cols))
-  }
-  
-  if (numPlots==1) {
-    print(plots[[1]])
-    
-  } else {
-    # Set up the page
-    grid.newpage()
-    pushViewport(viewport(layout = grid.layout(nrow(layout), ncol(layout))))
-    
-    # Make each plot, in the correct location
-    for (i in 1:numPlots) {
-      # Get the i,j matrix positions of the regions that contain this subplot
-      matchidx <- as.data.frame(which(layout == i, arr.ind = TRUE))
-      
-      print(plots[[i]], vp = viewport(layout.pos.row = matchidx$row,
-                                      layout.pos.col = matchidx$col))
-    }
-  }
-}
 
 
 #--- Plot
