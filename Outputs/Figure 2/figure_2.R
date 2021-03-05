31f92272016a00733eeca0bb7e3596ee7d05a85e
LoiseauN <nicolas.loiseau@cnrs.fr>
2021-03-05 16:33:26 +0100

    :fish: Data and code for figure 2

--- Outputs/Figure 2/figure_2.R
+++ Outputs/Figure 2/figure_2.R
@@ -1,1 +1,92 @@
-a
+
+## Import Icons ----
+
+paths <- list.files(path = here::here("Outputs", "Figure 2"), pattern = "*.png$", 
+                    full.names = TRUE)
+
+files <- list.files(path = here::here("data", "icons"), pattern = "*.png$", 
+                    full.names = FALSE)
+
+all_im <- lapply(paths, png::readPNG)
+names(all_im) <- gsub(".png", "", files)
+
+
+## Prepare Data ----
+
+
+
+filenames <- list.files(path = here::here("outputs"), pattern = "_res.rds$",
+                        full.names = FALSE)
+
+files <- list.files(path = here::here("outputs"), pattern = "_res.rds$",
+                    full.names = TRUE)
+list_res <- lapply(files, function(x) readRDS(x))
+
+
+
+
+
+lapply(file_names, load, environment())
+library(ecodist)
+library(betapart)
+library(ade4)
+library(vegan)
+library(sp)
+library(stringr)
+library(ggplot2)
+library(cluster)
+library(rgeos)
+library(sp)
+library(rgdal)
+library(maptools)
+library(png)
+
+library(grid)
+img <- readPNG("Fish.png")
+img2 <- readPNG("Bird.png")
+img3 <- readPNG("Plant.png")
+##################################################################################################################################### 
+##
+##
+##
+## FONCTION  multiplot
+##
+##
+###################################################################################################################################### 
+
+
+multiplot <- function(..., plotlist=NULL, file, cols=1, layout=NULL) {
+  library(grid)
+  
+  # Make a list from the ... arguments and plotlist
+  plots <- c(list(...), plotlist)
+  
+  numPlots = length(plots)
+  
+  # If layout is NULL, then use 'cols' to determine layout
+  if (is.null(layout)) {
+    # Make the panel
+    # ncol: Number of columns of plots
+    # nrow: Number of rows needed, calculated from # of cols
+    layout <- matrix(seq(1, cols * ceiling(numPlots/cols)),
+                     ncol = cols, nrow = ceiling(numPlots/cols))
+  }
+  
+  if (numPlots==1) {
+    print(plots[[1]])
+    
+  } else {
+    # Set up the page
+    grid.newpage()
+    pushViewport(viewport(layout = grid.layout(nrow(layout), ncol(layout))))
+    
+    # Make each plot, in the correct location
+    for (i in 1:numPlots) {
+      # Get the i,j matrix positions of the regions that contain this subplot
+      matchidx <- as.data.frame(which(layout == i, arr.ind = TRUE))
+      
+      print(plots[[i]], vp = viewport(layout.pos.row = matchidx$row,
+                                      layout.pos.col = matchidx$col))
+    }
+  }
+}
