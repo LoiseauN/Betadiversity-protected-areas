35fb2bd9826cd8272d1da709200dcfe5a22e4191
LoiseauN <nicolas.loiseau@cnrs.fr>
2021-03-11 18:26:37 +0100

    :boom:figure 7

--- Outputs/Figure_7/figure_7.R
+++ Outputs/Figure_7/figure_7.R
@@ -23,9 +23,14 @@ files <- list.files(path = here::here("Outputs", "Figure_7"), pattern = ".RData$
 lapply(files, load, environment())
 
 
-venn_plot <- grid.arrange(plot(venn_fish_all, quantities = list(col = "black", cex = 1),lty = c(3,2,1),fill = c("grey","cadetblue3","steelblue3"),labels = list(col = "black", cex = 1),alpha=c(0.1,0.3,0.6)),
-                          plot(venn_bird_all,quantities = list(col = "black", cex = 1),lty = c(3,2,1),fill = c("grey","chocolate1","coral1"),labels = list(col = "black", cex = 1),alpha=c(0.1,0.3,0.6)),
-                          plot(venn_plant_all, quantities = list(col = "black", cex = 1),lty = c(3,2,1),fill = c("grey","chartreuse2","chartreuse4"),labels = list(col = "black", cex = 1),alpha=c(0.1,0.3,0.6)),
+
+
+
+grDevices::pdf(file = here::here("Figures", "Figure7.pdf"), 
+               width = 11.7, height = 8.3) #SAVE A4
+grid.arrange(plot(venn_fish_all, quantities = list(col = "black", cex = 1),lty = c(3,2,1),fill = c("grey","cadetblue3","steelblue3"),labels = list(col = "black", cex = 1),alpha=c(0.1,0.3,0.6)),
+             plot(venn_bird_all,quantities = list(col = "black", cex = 1),lty = c(3,2,1),fill = c("grey","chocolate1","coral1"),labels = list(col = "black", cex = 1),alpha=c(0.1,0.3,0.6)),
+             plot(venn_plant_all, quantities = list(col = "black", cex = 1),lty = c(3,2,1),fill = c("grey","chartreuse2","chartreuse4"),labels = list(col = "black", cex = 1),alpha=c(0.1,0.3,0.6)),
              plot(venn_fish_THR, quantities = list(col = "black", cex = 1),lty = c(3,2,1),fill = c("grey","cadetblue3","steelblue3"),labels = list(col = "black", cex = 1),alpha=c(0.1,0.3,0.6)),
              plot(venn_bird_THR,quantities = list(col = "black", cex = 1),lty = c(3,2,1),fill = c("grey","chocolate1","coral1"),labels = list(col = "black", cex = 1),alpha=c(0.1,0.3,0.6)),
              plot(venn_plant_THR, quantities = list(col = "black", cex = 1),lty = c(3,2,1),fill = c("grey","chartreuse2","chartreuse4"),labels = list(col = "black", cex = 1),alpha=c(0.1,0.3,0.6)),
@@ -33,10 +38,5 @@ venn_plot <- grid.arrange(plot(venn_fish_all, quantities = list(col = "black", c
              plot(venn_bird_LR,quantities = list(col = "black", cex = 1),lty = c(3,2,1),fill = c("grey","chocolate1","coral1"),labels = list(col = "black", cex = 1),alpha=c(0.1,0.3,0.6)),
              plot(venn_plant_LR, quantities = list(col = "black", cex = 1),lty = c(3,2,1),fill = c("grey","chartreuse2","chartreuse4"),labels = list(col = "black", cex = 1),alpha=c(0.1,0.3,0.6)),
              nrow=3)
-
-
-grDevices::pdf(file = here::here("Figures", "Figure7.pdf"), 
-               width = 11.7, height = 8.3) #SAVE A4
-print(venn_plot)
 dev.off()
 
