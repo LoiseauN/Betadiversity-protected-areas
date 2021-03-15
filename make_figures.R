71d497696f8c8d10224b01f10447c5ad81af77f3
LoiseauN <nicolas.loiseau@cnrs.fr>
2021-03-15 15:46:45 +0100

    :boom: make_figures

---
+++ make_figures.R
@@ -0,0 +1,19 @@
+#' This script reproduces  figures of the Maximising regional biodiversity requires a mosaic of protection levels article.
+#'
+#' @author Nicolas LOISEAU, \email{nicolas.loiseau1@@gmail.com},
+#'
+#' @date 2021/03/16
+
+
+## Run Project ----
+source(here::here("Outputs","Figure_2.R", "figure_2.R")) 
+source(here::here("Outputs","Figure_3.R", "figure_3.R")) 
+source(here::here("Outputs","Figure_4.R", "figure_4.R")) 
+source(here::here("Outputs","Figure_5.R", "figure_5.R")) 
+source(here::here("Outputs","Figure_6.R", "figure_6.R")) 
+source(here::here("Outputs","Figure_7.R", "figure_7.R")) 
+source(here::here("Outputs","Figure_S1.R", "figure_S1.R"))
+source(here::here("Outputs", "Figure_S2.R","figure_S2.R"))
+source(here::here("Outputs","Figure_S3.R", "figure_S3.R"))
+source(here::here("Outputs", "Figure_S4.R","figure_S4.R"))
+source(here::here("Outputs", "Figure_S5.R","figure_S5.R"))
