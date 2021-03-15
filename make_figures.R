60684a4d41af394549de3e74fca14197ce702e4b
LoiseauN <nicolas.loiseau@cnrs.fr>
2021-03-15 15:47:01 +0100

    :boom: make figures

--- make_figures.R
+++ make_figures.R
@@ -1,3 +1,4 @@
+
 #' This script reproduces  figures of the Maximising regional biodiversity requires a mosaic of protection levels article.
 #'
 #' @author Nicolas LOISEAU, \email{nicolas.loiseau1@@gmail.com},
@@ -6,14 +7,14 @@
 
 
 ## Run Project ----
-source(here::here("Outputs","Figure_2.R", "figure_2.R")) 
-source(here::here("Outputs","Figure_3.R", "figure_3.R")) 
-source(here::here("Outputs","Figure_4.R", "figure_4.R")) 
-source(here::here("Outputs","Figure_5.R", "figure_5.R")) 
-source(here::here("Outputs","Figure_6.R", "figure_6.R")) 
-source(here::here("Outputs","Figure_7.R", "figure_7.R")) 
-source(here::here("Outputs","Figure_S1.R", "figure_S1.R"))
-source(here::here("Outputs", "Figure_S2.R","figure_S2.R"))
-source(here::here("Outputs","Figure_S3.R", "figure_S3.R"))
-source(here::here("Outputs", "Figure_S4.R","figure_S4.R"))
-source(here::here("Outputs", "Figure_S5.R","figure_S5.R"))
+source(here::here("Outputs","Figure_2", "figure_2.R")) 
+source(here::here("Outputs","Figure_3", "figure_3.R")) 
+source(here::here("Outputs","Figure_4", "figure_4.R")) 
+source(here::here("Outputs","Figure_5", "figure_5.R")) 
+source(here::here("Outputs","Figure_6", "figure_6.R")) 
+source(here::here("Outputs","Figure_7", "figure_7.R")) 
+source(here::here("Outputs","Figure_S1", "figure_S1.R"))
+source(here::here("Outputs", "Figure_S2","figure_S2.R"))
+source(here::here("Outputs","Figure_S3", "figure_S3.R"))
+source(here::here("Outputs", "Figure_S4","figure_S4.R"))
+source(here::here("Outputs", "Figure_S5","figure_S5.R"))
